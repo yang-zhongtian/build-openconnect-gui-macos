e9577bb2ad20e3265c103b2b153bd647be84712c
RichardYang <zhongtian.yang@qq.com>
2026-02-07 16:43:32 +0800

    Modify to build by actions

--- build-aarch64.sh
+++ build-aarch64.sh
@@ -2,6 +2,9 @@
 # this script builds OpenConnect-GUI for an Arm-based M-series Mac running at
 # least macOS 12
 
+# set build root directory
+BUILD_ROOT=${BUILD_ROOT:-~/Downloads/build-openconnect-gui}
+
 # get the current maximum amount of open file descriptors
 export ULIMIT_SAVED=$(ulimit -n)
 
@@ -25,37 +28,37 @@ export SDKROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platf
 # adjust the gnutls homebrew formula so that it is not configured to look in a
 # specific default trust store file. this should cause the macOS access
 # keychain to be used instead.
-mkdir -p ~/Downloads/build-openconnect-gui/rb-aarch64
+mkdir -p ${BUILD_ROOT}/rb-aarch64
 brew install gnutls
-cp /opt/homebrew/opt/gnutls/.brew/gnutls.rb ~/Downloads/build-openconnect-gui/rb-aarch64/
-sed -I '' -e '/--with-default-trust-store-file/d' ~/Downloads/build-openconnect-gui/rb-aarch64/gnutls.rb
+cp /opt/homebrew/opt/gnutls/.brew/gnutls.rb ${BUILD_ROOT}/rb-aarch64/
+sed -I '' -e '/--with-default-trust-store-file/d' ${BUILD_ROOT}/rb-aarch64/gnutls.rb
 brew uninstall gnutls
 
 # install modified gnutls formula
-brew install --build-from-source --formula ~/Downloads/build-openconnect-gui/rb-aarch64/gnutls.rb gnutls
+brew install --build-from-source --formula ${BUILD_ROOT}/rb-aarch64/gnutls.rb gnutls
 
 # adjust qt@6 homebrew formula to force target to macOS 12.0
-mkdir -p ~/Downloads/build-openconnect-gui/rb-aarch64
+mkdir -p ${BUILD_ROOT}/rb-aarch64
 brew install qt@6
-cp /opt/homebrew/opt/qt/.brew/qt.rb ~/Downloads/build-openconnect-gui/rb-aarch64/
-sed -I '' -e 's/-DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}.0/-DCMAKE_OSX_DEPLOYMENT_TARGET=12.0/' ~/Downloads/build-openconnect-gui/rb-aarch64/qt.rb
+cp /opt/homebrew/opt/qt/.brew/qt.rb ${BUILD_ROOT}/rb-aarch64/
+sed -I '' -e 's/-DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}.0/-DCMAKE_OSX_DEPLOYMENT_TARGET=12.0/' ${BUILD_ROOT}/rb-aarch64/qt.rb
 brew uninstall qt@6
 
 # install modified qt@6 formula
-brew install --build-from-source --formula ~/Downloads/build-openconnect-gui/rb-aarch64/qt.rb qt6
+brew install --build-from-source --formula ${BUILD_ROOT}/rb-aarch64/qt.rb qt6
 
 # install remaining dependencies
 brew install cmake openconnect spdlog vulkan-headers pkgconfig
 
 # get OpenConnect-GUI source code for v1.6.2
-cd ~/Downloads/build-openconnect-gui
+cd ${BUILD_ROOT}
 git clone https://gitlab.com/openconnect/openconnect-gui.git openconnect-gui-macos12-aarch64
 cd openconnect-gui-macos12-aarch64
 git tag
 git checkout tags/v1.6.2
 
 # modify deployment target from 10.12 to 12.0
-sed -I '' -e 's/CMAKE_OSX_DEPLOYMENT_TARGET "10.12"/CMAKE_OSX_DEPLOYMENT_TARGET "12.0"/' ~/Downloads/build-openconnect-gui/openconnect-gui-macos12-aarch64/CMakeLists.txt
+sed -I '' -e 's/CMAKE_OSX_DEPLOYMENT_TARGET "10.12"/CMAKE_OSX_DEPLOYMENT_TARGET "12.0"/' ${BUILD_ROOT}/openconnect-gui-macos12-aarch64/CMakeLists.txt
 
 # configure and build
 /opt/homebrew/bin/cmake .
